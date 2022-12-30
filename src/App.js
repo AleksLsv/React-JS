49f32f5b4c95549a1d7a496de6677b21422c7b55
Aleks <jooksja1@gmail.com>
2022-12-31 01:03:36 +0200

    20 lessons is done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import React from "react";
 import './App.css';
-
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import s from "./components/NavBar/NavBar.module.css";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
-function App() {
+function App(props) {
   return (
+      <BrowserRouter>
     <div className='app-wrapper'>
         <Header />
         <NavBar />
-        <Profile />
+        <div className='app-wrapper-content'>
+            <Routes>
+                <Route path="/profile" element={<Profile/>}/>
+                <Route path="/dialogs" element={<Dialogs/>}/>
+            </Routes>
 
+        </div>
     </div>
+   </BrowserRouter>
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,34 @@
-//import classes from './NavBar.module.css';
+import React from "react";
 import s from './NavBar.module.css';
-
+import {NavLink} from "react-router-dom";
+//import classes from './NavBar.module.css';
+// s - classes
 
 
 function NavBar() {
-    return (<nav className={s.nav}>
-        <div>
-            <a> Profile </a>
-        </div>
+    return (
+        <nav className={s.nav}>
+            <div>
+                <NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }> Profile </NavLink>
+            </div>
 
-        <div className={`${s.item} ${s.active}`}>
-            <a> Messages </a>
-        </div>
+            <div>
+                <NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }> Messages </NavLink>
+            </div>
 
-        <div className={s.item}>
-            <a> News </a>
-        </div>
+            <div>
+                <NavLink to="/news" className = { navData => navData.isActive ? s.active : s.item }> News </NavLink>
+            </div>
 
-        <div className={s.item}>
-            <a> Music </a>
-        </div>
-        <div className={s.item}>
-            <a> Settings </a>
-        </div>
+            <div>
+                <NavLink to="/music" className = { navData => navData.isActive ? s.active : s.item }> Music </NavLink>
+            </div>
+            <div>
+                <NavLink to="/settings" className = { navData => navData.isActive ? s.active : s.item }> Settings </NavLink>
+            </div>
 
-    </nav>);
+        </nav>
+    );
 }
+
 export default NavBar;
