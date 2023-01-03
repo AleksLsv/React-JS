de0887baad0c18afb4ace393699d18c878655fbe
Aleks <jooksja1@gmail.com>
2023-01-03 13:33:37 +0200

    add textarea

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -10,13 +10,36 @@ function Dialogs(props) {
 
     let messagesElements = props.state.messages.map(m => <Message message={m.message}/>);
 
+    let newMessagesElement = React.createRef();
+
+    function addMessage() {
+        let text = newMessagesElement.current.value;
+        alert(text);
+    }
+
+
+
     return (
         <div className={s.dialogs}>
+
             <div className={s.dialogsItems}>
                 {dialogsElements}
             </div>
+
+
             <div className={s.messages}>
-             {messagesElements}
+
+                {messagesElements}
+
+            <div>
+                <textarea ref = {newMessagesElement}></textarea>
+            </div>
+            <div>
+                <button onClick={addMessage}>Send</button>
+            </div>
+
+
+
             </div>
         </div>
     );
