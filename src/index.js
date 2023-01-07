d01c77801011fceab2b7a20ccefb860f7cbbb680
Aleks <jooksja1@gmail.com>
2023-01-07 16:28:01 +0200

    37 lesson done

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, {subscribe, updateNewPostText} from "./redux/state";
-import {addPost} from "./redux/state";
+import store from "./redux/state";
 
 //addPost('SamuraiJS.com');
 
@@ -12,20 +11,22 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderEntireTree = (state) => {
     root.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost} updateNewPostText = {updateNewPostText}/>
+            <App state={state} addPost={store.addPost.bind(store)}
+                 updateNewPostText = {store.updateNewPostText.bind(store)}/>
         </React.StrictMode>
     );
 }
 
+rerenderEntireTree(store.getState());
 
-rerenderEntireTree(state);
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-subscribe(rerenderEntireTree);
+
 
 // сделали callback, в функцию subscribe из state вставляем функцию перерисовки
 // дерева rerenderEntireTree
