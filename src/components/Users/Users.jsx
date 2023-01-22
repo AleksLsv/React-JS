ab41a393b483c763d033a7f93ab5be9a9976121a
Aleks <jooksja1@gmail.com>
2023-01-22 12:10:33 +0200

    56 lesson is done

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,51 +1,34 @@
-import React from "react";
-import styles from './Users.module.css';
-import axios from "axios";
-import userPhoto from '../../assets/images/user.jpg';
+import React from 'react';
+import styles from "./Users.module.css";
+import userPhoto from "../../assets/images/user.jpg";
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
-            });
+let Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pagesCount2;
+
+    if (pagesCount <10)
+        pagesCount2 = pagesCount;
+    else
+        pagesCount2 = 10;
+
+    let pages = [];
+
+    for (let i = 1; i <= pagesCount2; i++) {
+        pages.push(i);
     }
 
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-            });
-    }
+    return <div>
+        <div>
+            {pages.map(p => {
+                return <span className={props.currentPage === p && styles.selectedPage}
+                             onClick={(e) => {
+                                 props.onPageChanged(p)
+                             }}>{p}</span>
+            })}
+        </div>
 
-    render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-        let pagesCount2;
-
-        if (pagesCount <10)
-            pagesCount2 = pagesCount;
-         else
-            pagesCount2 = 10;
-
-        let pages = [];
-
-        for (let i = 1; i <= pagesCount2; i++) {
-            pages.push(i);
-        }
-        return <div>
-            <div>
-                {pages.map(p => {
-                    return <span className={this.props.currentPage === p && styles.selectedPage}
-                                 onClick={(e) => {
-                                     this.onPageChanged(p)
-                                 }}>{p}</span>
-                })}
-            </div>
-
-            {
-                this.props.users.map(u => <div key={u.id}>
+        {
+            props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}
@@ -55,17 +38,17 @@ class Users extends React.Component {
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                this.props.unfollow(u.id)
+                                props.unfollow(u.id)
                             }}> Unfollow </button>
                             : <button onClick={() => {
-                                this.props.follow(u.id)
+                                props.follow(u.id)
                             }}> Follow </button>}
 
                     </div>
 
                 </span>
 
-                    <span>
+                <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status} </div>
@@ -77,14 +60,13 @@ class Users extends React.Component {
                 </span>
 
 
-                </div>)
+            </div>)
 
 
-            }
+        }
 
 
-        </div>
-    }
+    </div>
 
 }
 
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -9,7 +9,7 @@ let initialState = {
     users: [ ],
     pageSize: 10,
     totalUsersCount: 0,
-    currentPage: 3
+    currentPage: 1
 };
 
 
