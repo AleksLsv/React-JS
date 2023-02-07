15abb27fb7a99c4c02c0e533491bfda8fdd02221
Aleks <jooksja1@gmail.com>
2023-02-07 19:19:30 +0200

    81b lesson done, 80 propuschen

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,32 +6,26 @@ import {NavLink} from "react-router-dom";
 function NavBar() {
     let navDataIsActive = navData => navData.isActive ? s.active : s.item;
 
+    let linksData = [
+        {url: '/profile', navBarItem: 'Profile'},
+        {url: '/dialogs', navBarItem: 'Messages'},
+        {url: '/users', navBarItem: 'Users'},
+        {url: '/news', navBarItem: 'News'},
+        {url: '/music', navBarItem: 'Music'},
+        {url: '/settings', navBarItem: 'Settings'}
+    ];
+
+    let linkElements = linksData.map(el => {
+        return (
+            <div>
+                <NavLink to = {el.url} className={navDataIsActive}> {el.navBarItem} </NavLink>
+            </div>
+        )
+    });
+
     return (
         <nav className={s.nav}>
-            <div>
-                <NavLink to="/profile" className = {navDataIsActive}> Profile </NavLink>
-            </div>
-
-            <div>
-                <NavLink to="/dialogs" className = {navDataIsActive }> Messages </NavLink>
-            </div>
-
-            <div>
-                <NavLink to="/users" className = {navDataIsActive}> Users </NavLink>
-            </div>
-
-            <div>
-                <NavLink to="/news" className = {navDataIsActive}> News </NavLink>
-            </div>
-
-            <div>
-                <NavLink to="/music" className = {navDataIsActive}> Music </NavLink>
-            </div>
-            <div>
-                <NavLink to="/settings" className = {navDataIsActive}> Settings </NavLink>
-            </div>
-
-
+            {linkElements}
         </nav>
     );
 }
