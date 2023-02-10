e3b1730368c3600546549913f436fae15aeafc46
Aleks <jooksja1@gmail.com>
2023-02-10 11:00:31 +0200

    81b lesson done, 80 propuschen

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,18 +7,18 @@ function NavBar() {
     let navDataIsActive = navData => navData.isActive ? s.active : s.item;
 
     let linksData = [
-        {url: '/profile', navBarItem: 'Profile'},
-        {url: '/dialogs', navBarItem: 'Messages'},
-        {url: '/users', navBarItem: 'Users'},
-        {url: '/news', navBarItem: 'News'},
-        {url: '/music', navBarItem: 'Music'},
-        {url: '/settings', navBarItem: 'Settings'}
+        {url: 'profile', navBarItem: 'Profile'},
+        {url: 'dialogs', navBarItem: 'Messages'},
+        {url: 'users', navBarItem: 'Users'},
+        {url: 'news', navBarItem: 'News'},
+        {url: 'music', navBarItem: 'Music'},
+        {url: 'settings', navBarItem: 'Settings'}
     ];
 
     let linkElements = linksData.map(el => {
         return (
             <div>
-                <NavLink to = {el.url} className={navDataIsActive}> {el.navBarItem} </NavLink>
+                <NavLink to = {'/' + el.url}  className={navDataIsActive}> {el.navBarItem} </NavLink>
             </div>
         )
     });
