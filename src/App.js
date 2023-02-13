dbb97f78d43ec21d1867566bc0e24340a17f5378
Aleks <jooksja1@gmail.com>
2023-02-13 23:02:24 +0200

    60 lesson, 2d time

--- src/App.js
+++ src/App.js
@@ -17,8 +17,10 @@ function App() {
         <NavBar />
         <div className='app-wrapper-content'>
             <Routes>
-                <Route path="/profile/:userId" element={<ProfileContainer />}/>
+                <Route path="/profile/:userId?" element={<ProfileContainer />}/>
+{/*
                 <Route path="/profile/*" element={<ProfileContainer />}/>
+*/}
                 <Route path="/messages/*" element={<DialogsContainer />}/>
                 <Route path="/users" element={<UsersContainer />}/>
                 <Route path="/login" element={<LoginPage />}/>
